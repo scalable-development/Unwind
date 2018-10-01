4faf49b8bb793785350e07cf1b4154bb8b866132
Rahel Lüthy <rahel.luethy@gmail.com>
2018-10-01 11:32:25 +0200

    Create config in constructor

--- Unwind/BubblesView.swift
+++ Unwind/BubblesView.swift
@@ -11,23 +11,47 @@ import UIKit
 @IBDesignable
 class BubblesView: UIView {
     
-    let bubbleSize: CGFloat = 10
+    static let bubbleSize: CGFloat = 20
+    
+    struct BubbleConfig {
+        let bubbleCountX: CGFloat
+        let bubbleCountY: CGFloat
+        let start: CGPoint
+    }
+    
+    private let config: BubbleConfig
+    
+    fileprivate static func createBubbleConfig(_ rect: CGRect) -> BubbleConfig {
+        let bubbleCountX = floor(rect.width / BubblesView.bubbleSize)
+        let bubbleCountY = floor(rect.height / BubblesView.bubbleSize)
+        return BubbleConfig(
+            bubbleCountX: bubbleCountX,
+            bubbleCountY: bubbleCountY,
+            start: CGPoint(
+                x: (rect.width - (bubbleCountX * BubblesView.bubbleSize)) / 2,
+                y: (rect.height - (bubbleCountY * BubblesView.bubbleSize)) / 2
+            )
+        )
+    }
+    
+    override init(frame: CGRect) {
+        config = BubblesView.createBubbleConfig(frame)
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        config = BubblesView.createBubbleConfig(UIScreen.main.bounds)
+        super.init(coder: aDecoder)
+    }
 
     override func draw(_ rect: CGRect) {
-        
-        let bubbleCountX = floor(rect.width / bubbleSize)
-        let bubbleCountY = floor(rect.height / bubbleSize)
-        
-        let startX = (rect.width - (bubbleCountX * bubbleSize)) / 2
-        let startY = (rect.height - (bubbleCountY * bubbleSize)) / 2
-        
-        for x in stride(from: 0, to: bubbleCountX, by: 1) {
-            for y in stride(from: 0, to: bubbleCountY, by: 1) {
+        for x in stride(from: 0, to: self.config.bubbleCountX, by: 1) {
+            for y in stride(from: 0, to: self.config.bubbleCountY, by: 1) {
                 let cell = CGRect(
-                    x: startX + x * bubbleSize,
-                    y: startY + y * bubbleSize,
-                    width: bubbleSize,
-                    height: bubbleSize
+                    x: self.config.start.x + x * BubblesView.bubbleSize,
+                    y: self.config.start.y + y * BubblesView.bubbleSize,
+                    width: BubblesView.bubbleSize,
+                    height: BubblesView.bubbleSize
                 )
                 UIColor.purple.setFill()
                 UIRectFill(cell)
